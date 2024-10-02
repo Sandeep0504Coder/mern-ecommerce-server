7b5d06d1f07f2743614ab6c90a5d964b78d3c2af
Sandeep Mandal <sandeepmandal0504@gmail.com>
2024-10-03 00:34:44 +0530

    Code modification

--- src/routes/statsRoute.ts
+++ src/routes/statsRoute.ts
@@ -5,7 +5,7 @@ import { getBarCharts, getDashboardStats, getLineCharts, getPieCharts } from "..
 const app = express.Router();
 
 //route - api/v1/dashboard/stats
-app.get( "/stats", getDashboardStats );
+app.get( "/stats", adminOnly, getDashboardStats );
 
 //route - api/v1/dashboard/pie
 app.get( "/pie", adminOnly, getPieCharts );
